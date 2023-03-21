20d8ca83913a6cd9a02b849a3be12cd4c2766e7e
Dmitry Kulinenko <67313082+Dmitry-Kulinenko@users.noreply.github.com>
2023-03-21 14:21:34 +0300

    added new converter in ConverterConfig

--- src/main/java/by/itacademy/fitness/config/ConverterConfig.java
+++ src/main/java/by/itacademy/fitness/config/ConverterConfig.java
@@ -2,8 +2,10 @@ package by.itacademy.fitness.config;
 
 import by.itacademy.fitness.core.converter.UserCreateUpdateDTOToEntityConverter;
 import by.itacademy.fitness.core.converter.UserEntityToDTOConverter;
+import by.itacademy.fitness.core.converter.UserRegistrationDTOToEntityConverter;
 import by.itacademy.fitness.core.dto.UserCreateUpdateDTO;
 import by.itacademy.fitness.core.dto.UserDTO;
+import by.itacademy.fitness.core.dto.UserRegistrationDTO;
 import by.itacademy.fitness.dao.entity.User;
 import by.itacademy.fitness.service.api.IRoleService;
 import by.itacademy.fitness.service.api.IStatusService;
@@ -14,13 +16,17 @@ import org.springframework.core.convert.converter.Converter;
 @Configuration
 public class ConverterConfig {
     @Bean
-    public Converter<UserCreateUpdateDTO, User> userToEntityConverter(IRoleService roleServise,
-                                                                      IStatusService statusService) {
-        return new UserCreateUpdateDTOToEntityConverter(roleServise, statusService);
+    public Converter<UserCreateUpdateDTO, User> userCreateUpdateDTOToEntityConverter(IRoleService roleService, IStatusService statusService) {
+        return new UserCreateUpdateDTOToEntityConverter(roleService, statusService);
     }
 
     @Bean
     public Converter<User, UserDTO> userEntityToDTOConverter() {
         return new UserEntityToDTOConverter();
+    }
+
+    @Bean
+    public Converter<UserRegistrationDTO, User> userRegistrationDTOToUserConverter(IRoleService roleService, IStatusService statusService) {
+        return new UserRegistrationDTOToEntityConverter(roleService, statusService);
     }
 }
