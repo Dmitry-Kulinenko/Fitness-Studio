e9f14fc179d7402fece8a8a8b2c5390f47e17bcc
Dmitry Kulinenko <67313082+Dmitry-Kulinenko@users.noreply.github.com>
2023-03-21 14:22:41 +0300

    removed redundant repository

--- src/main/java/by/itacademy/fitness/dao/repository/IUserPersonalAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package by.itacademy.fitness.dao.repository;
-
-import by.itacademy.fitness.dao.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface IUserPersonalAccountRepository extends CrudRepository<User, UUID> {
-}
