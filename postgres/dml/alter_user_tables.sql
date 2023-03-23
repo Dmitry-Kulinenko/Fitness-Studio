bc5eaa0b402a85217af76ed6deed5c42ce046c1c
Dmitry Kulinenko <67313082+Dmitry-Kulinenko@users.noreply.github.com>
2023-03-23 17:10:48 +0300

    fixed ddl file

--- postgres/dml/alter_user_tables.sql
+++ postgres/dml/alter_user_tables.sql
@@ -1,9 +1,11 @@
-INSERT INTO fitness.user_role (id, role)
+INSERT INTO fitness.user_role(id, role)
 VALUES (1, 'ADMIN'),
        (2, 'USER');
 INSERT INTO fitness.user_status(id, status)
 VALUES (1, 'WAITING_ACTIVATION'),
        (2, 'ACTIVATED'),
        (3, 'DEACTIVATED');
-INSERT INTO fitness.user(uuid,dt_create, dt_update, mail, fio, password, user_status, user_role)
-VALUES ('3bd6f642-c31b-11ed-afa1-0242ac120002',NOW(), NOW(), 'kulinenko34@gmail.com', 'Admin', 11, 2, 1)
+INSERT INTO fitness.user(id, dt_create, dt_update,
+                         mail, fio, password, status, role)
+VALUES ('3bd6f642-c31b-11ed-afa1-0242ac120002', NOW(), NOW(),
+        'kulinenko34@gmail.com', 'Admin', 11, 2, 1);
