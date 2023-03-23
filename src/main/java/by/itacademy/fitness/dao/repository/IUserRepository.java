61ff15121531c403466618811396b850198b9bde
Dmitry Kulinenko <67313082+Dmitry-Kulinenko@users.noreply.github.com>
2023-03-23 17:16:13 +0300

    fixed update method

--- src/main/java/by/itacademy/fitness/dao/repository/IUserRepository.java
+++ src/main/java/by/itacademy/fitness/dao/repository/IUserRepository.java
@@ -3,28 +3,30 @@ package by.itacademy.fitness.dao.repository;
 import by.itacademy.fitness.dao.entity.Role;
 import by.itacademy.fitness.dao.entity.Status;
 import by.itacademy.fitness.dao.entity.User;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface IUserRepository extends CrudRepository<User, UUID>, PagingAndSortingRepository<User, UUID> {
     @Transactional
-    @Modifying
+    @Modifying(clearAutomatically = true)
     @Query("""
-            update User u set u.mail = ?1, u.password = ?2, u.fullName = ?3, u.role = ?4, u.status = ?5
-            where u.uuid = ?6 and u.updateDateTime = ?7""")
-    void updateUserData(String mail,
-                        String password,
-                        String fullName,
-                        Role role,
-                        Status status,
-                        UUID uuid,
-                        LocalDateTime updateDateTime);
+            update User u set u.mail = ?1, u.password = ?2, u.fullName = ?3, u.role = ?4, u.status = ?5, 
+            u.updateDateTime=?6
+            where u.uuid = ?7 and u.updateDateTime=?8""")
+    int updateUser(String mail,
+                   String password,
+                   String fullName,
+                   Role role,
+                   Status status,
+                   LocalDateTime newUpdateDateTime,
+                   UUID uuid,
+                   LocalDateTime updateDateTime);
 
 
 }
