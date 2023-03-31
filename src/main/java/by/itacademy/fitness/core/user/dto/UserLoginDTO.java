c33026784e8d0ac23b73f856c8e6fa85a1a7a3db
Dmitry Kulinenko <67313082+Dmitry-Kulinenko@users.noreply.github.com>
2023-03-31 11:28:26 +0300

    added UserLoginDTO

--- src/main/java/by/itacademy/fitness/core/user/dto/UserLoginDTO.java
+++ src/main/java/by/itacademy/fitness/core/user/dto/UserLoginDTO.java
@@ -1,4 +1,23 @@
 package by.itacademy.fitness.core.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class UserLoginDTO {
+    @NotBlank(message = "Mail couldn't be blank")
+    private final String mail;
+    @NotBlank(message = "Password couldn't be blank")
+    private final String password;
+
+    public UserLoginDTO(String mail, String password) {
+        this.mail = mail;
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
