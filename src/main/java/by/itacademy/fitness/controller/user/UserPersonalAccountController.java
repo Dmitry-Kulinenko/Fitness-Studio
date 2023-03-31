84f73b04714abeb486be61e90b246916f16dae51
Dmitry Kulinenko <67313082+Dmitry-Kulinenko@users.noreply.github.com>
2023-03-31 11:27:27 +0300

    added methods implementation to a personal account controller and service

--- src/main/java/by/itacademy/fitness/controller/user/UserPersonalAccountController.java
+++ src/main/java/by/itacademy/fitness/controller/user/UserPersonalAccountController.java
@@ -1,45 +1,55 @@
 package by.itacademy.fitness.controller.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import by.itacademy.fitness.core.user.dto.UserDTO;
+import by.itacademy.fitness.core.user.dto.UserLoginDTO;
+import by.itacademy.fitness.core.user.dto.UserRegistrationDTO;
+import by.itacademy.fitness.service.user.api.IUserPersonalAccountService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserPersonalAccountController {
+    private final IUserPersonalAccountService userPersonalAccountService;
+
+    public UserPersonalAccountController(IUserPersonalAccountService userPersonalAccountService) {
+        this.userPersonalAccountService = userPersonalAccountService;
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<String> register(@RequestBody UserRegistrationDTO userRegistration) {
+        userPersonalAccountService.register(userRegistration);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
+    }
+
+    @GetMapping("/verification")
+    public ResponseEntity<String> verifyCode(@RequestParam String code,
+                                             @RequestParam String mail) {
+
+        userPersonalAccountService.verify(code, mail);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody UserLoginDTO userLogin) {
 //
-//    @PostMapping("/registration")
-//    public ResponseEntity<String> register(@RequestBody UserRegistrationDTO userRegistration) {
-//
-//        this.userAuthenticationService.register(userRegistration);
-//        return ResponseEntity
-//                .status(HttpStatus.CREATED)
-//                .build();
-//    }
-//    @GetMapping("/verification")
-//    public ResponseEntity<String> verifyCode(@RequestParam String code,
-//                                             @RequestParam String mail) {
-//
-//        this.userAuthenticationService.verify(code, mail);
-//        return ResponseEntity.ok().build();
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<String> login(@Valid @RequestBody UserLoginDTO userLogin) {
-//
-//        if (!this.userDataService.isActivated(userLogin.getMail())) {
+//        if (!userDataService.isActivated(userLogin.getMail())) {
 //            throw new BadCredentialsException(userLogin.getMail()
 //                    + " mail address has not been verified!");
 //        }
-//        String jwtToken = this.userAuthenticationService.login(userLogin);
-//        return ResponseEntity
-//                .status(HttpStatus.CREATED)
-//                .body(jwtToken);
-//    }
-//
-//    @GetMapping("/me")
-//    public ResponseEntity<UserDTO> getPersonalData() {
-//
-//        UserDTO userData = this.userAuthenticationService.getPersonalData();
-//        return ResponseEntity.ok(userData);
-//    }
+        String jwtToken = userPersonalAccountService.login(userLogin);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(jwtToken);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDTO> getPersonalData() {
+        UserDTO userData = userPersonalAccountService.getPersonalData();
+        return ResponseEntity.ok(userData);
+    }
 }
--- src/main/java/by/itacademy/fitness/service/user/impl/UserPersonalAccountService.java
+++ src/main/java/by/itacademy/fitness/service/user/impl/UserPersonalAccountService.java
@@ -1,36 +1,99 @@
 package by.itacademy.fitness.service.user.impl;
 
 import by.itacademy.fitness.core.user.dto.UserDTO;
+import by.itacademy.fitness.core.user.dto.UserDetailsDTO;
 import by.itacademy.fitness.core.user.dto.UserLoginDTO;
 import by.itacademy.fitness.core.user.dto.UserRegistrationDTO;
+import by.itacademy.fitness.dao.user.entity.User;
+import by.itacademy.fitness.dao.user.entity.userenum.StatusEnum;
+import by.itacademy.fitness.dao.user.repository.IStatusRepository;
 import by.itacademy.fitness.dao.user.repository.IUserRepository;
+import by.itacademy.fitness.security.JwtTokenUtil;
+import by.itacademy.fitness.security.UserHolder;
 import by.itacademy.fitness.service.user.api.IUserPersonalAccountService;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserPersonalAccountService implements IUserPersonalAccountService {
 
-    private IUserRepository userRepository;
+    private final Converter<UserRegistrationDTO, User> registrationDTOToEntityConverter;
+    private final Converter<User, UserDetailsDTO> entityToDetailsDTOConverter;
+    private final Converter<User, UserDTO> userEntityToDTOConverter;
+    private final PasswordEncoder passwordEncoder;
+    private final UserHolder userHolder;
+    private final JwtTokenUtil tokenUtil;
+    private final IUserRepository userRepository;
+    private final IStatusRepository statusRepository;
+//    private final IMailService mailService;
 
-    public UserPersonalAccountService(IUserRepository userRepository) {
+
+    public UserPersonalAccountService(Converter<UserRegistrationDTO, User> registrationDTOToEntityConverter,
+                                      Converter<User, UserDetailsDTO> entityToDetailsDTOConverter,
+                                      Converter<User, UserDTO> userEntityToDTOConverter,
+                                      PasswordEncoder passwordEncoder,
+                                      UserHolder userHolder,
+                                      JwtTokenUtil tokenUtil,
+                                      IUserRepository userRepository,
+                                      IStatusRepository statusRepository) {
+        this.registrationDTOToEntityConverter = registrationDTOToEntityConverter;
+        this.entityToDetailsDTOConverter = entityToDetailsDTOConverter;
+        this.userEntityToDTOConverter = userEntityToDTOConverter;
+        this.passwordEncoder = passwordEncoder;
+        this.userHolder = userHolder;
+        this.tokenUtil = tokenUtil;
         this.userRepository = userRepository;
+        this.statusRepository = statusRepository;
     }
 
     @Override
-    public void register(UserRegistrationDTO userRegistrationDTO) {
-
+    public void register(UserRegistrationDTO registrationDTO) {
+//        if (repository.existsByMail(user.getMail())) {
+//            throw new SingleErrorResponse("error", "user with this mail already exist");
+//        }
+        User user = registrationDTOToEntityConverter.convert(registrationDTO);
+        userRepository.save(user);
     }
 
     @Override
     public void verify(String code, String mail) {
+        User user = userRepository.findByMail(mail).orElseThrow(() ->
+                new IllegalArgumentException("user with this mail: " + mail
+                        + " not found"));
 
+//        if (mailService.checkVerification(mail, code)) {
+        user.setStatus(statusRepository.findByStatusName(StatusEnum.ACTIVATED).get());
+        userRepository.save(user);
+//        } else {
+//            throw new SingleErrorResponse("error", "invalid verification code");
+//        }
     }
 
     @Override
-    public String login(UserLoginDTO userLoginDTO) {
-        return null;
+    public String login(UserLoginDTO user) {
+        User entity = userRepository.findByMail(user.getMail()).orElseThrow(() ->
+                new IllegalArgumentException("user with email: " + user.getMail()
+                        + " not found"));
+        if (entity.getStatus().getStatusName() == StatusEnum.WAITING_ACTIVATION) {
+            throw new IllegalArgumentException("authorization is not available," +
+                    " the account is not verified");
+        }
+        if (passwordEncoder.matches(user.getPassword(), entity.getPassword())) {
+            UserDetailsDTO userDetails = entityToDetailsDTOConverter.convert(entity);
+            return tokenUtil.generateAccessToken(userDetails);
+        } else {
+            throw new IllegalArgumentException("invalid password");
+        }
     }
 
     @Override
     public UserDTO getPersonalData() {
-        return null;
+        User user = userRepository.findByMail(
+                        userHolder.getUser().getUsername())
+                .orElseThrow(() ->
+                        new IllegalArgumentException("user with this email not found"));
+        return userEntityToDTOConverter.convert(user);
     }
+
 }
